b1a888e9c7a9cf79ee668797c550f81ccbceaec2
CityMiles <47606156+CityMiles@users.noreply.github.com>
2019-06-24 12:33:41 -0700

    Update good.at.stuff.js

--- good.at.stuff/good.at.stuff.js
+++ good.at.stuff/good.at.stuff.js
@@ -7,7 +7,7 @@ buttonPress = () => {
 }
 
 breakingStory = (string) => {
-    return `Happy Friday! Just another awesome day in ${string}. =)`
+    return `Happy Monday! Just another awesome day in ${string}. =)`
 }
 
 dispRules = () => {
